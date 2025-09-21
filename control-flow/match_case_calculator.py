569ad7240ccf8c7459591568de768de323c2741f
giftbatolu <sekinatoyelami@gmail.com>
2025-09-21 22:22:13 +0100

    File: modified the  match_case_calculator.py file

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -3,14 +3,14 @@ num2 = int(input("Enter the second number: "))
 operator = input("Choose the operation (+, -, *, /): ")
 
 match operator:
-    case "*":
-        result = num1 * num2
-        print(f"The result is {result}")
     case "+":
         result = num1 + num2
         print(f"The result is {result}")
     case "-":
         result = num1 - num2
+        print(f"The result is {result}")
+    case "*":
+        result = num1 * num2
         print(f"The result is {result}")
     case "/":
         if num2 == 0:
