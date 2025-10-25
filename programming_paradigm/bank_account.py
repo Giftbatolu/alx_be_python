b93edf0dfe9f9338ee452957aa2d778adde5e021
giftbatolu <sekinatoyelami@gmail.com>
2025-10-25 16:51:10 +0100

    File: add bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,13 +1,13 @@
 class BankAccount:
-    def __init__(self, initial_balance = 0):
-        self.__account_balance = initial_balance
+    def __init__(self, account_balance = 0):
+        self.account_balance = account_balance
 
     def deposit(self, amount):
-        self.__account_balance = self.__account_balance + amount
+        self.account_balance = self.account_balance + amount
 
     def withdraw(self, amount):
         if amount <= self.account_balance:
-            self.__account_balance -= amount
+            self.account_balance -= amount
             return True
         else:
             False
