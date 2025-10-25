15e6505ca951bdf05f46e1b3b521ed49b589a3c2
giftbatolu <sekinatoyelami@gmail.com>
2025-10-25 17:07:07 +0100

    File: add robust_division_calculator.py

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,9 +1,7 @@
 def safe_divide(numerator, denominator):
     try:
-        num = float(numerator)
-        den = float(denominator)
-        result = num / den
-        return f"The result of the division is  {result:.1f}"
+        result = float(numerator)/float(denominator)
+        return f"The result of the division is  {result:.2f}"
     except ValueError:
         return "Error: Please enter numeric values only." 
     except ZeroDivisionError:
